e62da453f25958bdfa168dde72972c79e8b2ee31
Paulo Santana (PH) <paulo.santana@xpi.com.br>
2021-04-22 17:45:03 -0300

    fix program

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,20 +9,10 @@ namespace GradeBook
         {
             var book = new Book("PH book");
             book.AddGrade(89.1);
-            Console.WriteLine($"grade: {book.grades[0]}");
+            book.AddGrade(90.50);
+            book.AddGrade(77.5);
 
-            var grade = new List<double>() {12.7, 10.3, 6.11,4.1};
-
-            var result = 0.0;
-
-            foreach(var number in grade)
-            {
-                result += number;
-            }
-
-            result /= grade.Count;
-
-            Console.WriteLine($"Resultado: {result:N1}");
+            book.ShowStats();
         }
     }
 }
